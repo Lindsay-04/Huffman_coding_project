80e06d0123d33a4694e59d5bf0fc93b6ee31829a
Lindsay Rebeau <lindsay.rebeau@efrei.net>
2020-11-29 23:52:46 +0100

    Add status.h file

---
+++ status.h
@@ -0,0 +1,24 @@
+/*****************************************************************//**
+ * \file   status.h
+ * \brief  Header of the library which allows the management of the tests.
+ *
+ * \author Lindsay REBEAU lindsay.rebeau@efrei.net
+ * \date   November 2020
+ *********************************************************************/
+
+#ifndef STATUS_H_INCLUDED
+#define STATUS_H_INCLUDED
+
+/**
+ * \enum Status
+ * \brief an enum to check the correct functioning of the program.
+ */
+typedef enum {
+
+    OK = 0,
+    ERREUR = 1
+
+} Status;
+
+
+#endif // STATUS_H_INCLUDED
