1fe11641a23872dbd2ec6911cc3e4cbd7b41d4a0
Lindsay Rebeau <lindsay.rebeau@efrei.net>
2020-11-30 01:23:08 +0100

    Add a C source file

---
+++ HuffmanCodeNaiveVersion/Part 2.c
@@ -0,0 +1,144 @@
+#include "Part 2.h"
+
+LinkedList* create_element(char new_character){
+
+    return NULL;
+
+}
+
+LinkedList* add_element_start(LinkedList* start_list, LinkedList* new_element){
+
+    return NULL;
+
+}
+
+int position(const LinkedList* linkedList, const char x){
+
+    return 0;
+
+}
+
+int number_of_occurrences_file(const char* name_file, char searched_character){
+
+    return 0;
+
+}
+
+LinkedList* characters_occurrences_text(LinkedList* linkedList, LinkedList* new_element, const char* name_file){
+
+    return NULL;
+
+}
+
+void display_list(const LinkedList* linkedList){
+
+
+
+}
+
+void display_occurrences(LinkedList* linkedList){
+
+
+
+}
+
+void free_list(LinkedList* linkedList){
+
+
+
+}
+
+Node* create_node(char value){
+
+    return NULL;
+
+}
+
+void add_node(Node** tree, char value){
+
+
+
+}
+
+void display_tree(Node* tree){
+
+
+
+}
+
+void free_tree(Node* tree){
+
+
+
+}
+
+Queue* create_queue(){
+
+    return NULL;
+
+}
+
+int is_empty_queue(Queue* q){
+
+    return 0;
+
+}
+
+void enqueue(Queue* q, Node* value){
+
+
+
+}
+
+Node* dequeue(Queue* q){
+
+    return NULL;
+
+}
+
+void display_queue(Queue* q){
+
+
+
+}
+
+LinkedList* delete_element_linkedlist(LinkedList* linkedList, const char x){
+
+    return NULL;
+
+}
+
+LinkedList* min_list(LinkedList* linkedList){
+
+    return NULL;
+
+}
+
+Node* create_Huffman_tree(LinkedList* linkedList){
+
+    return NULL;
+
+}
+
+Node* Huffman_dictionary (const char* name_file, const char string_array[]){
+
+    return NULL;
+
+}
+
+void translate_file_dictionary(const char* name_source_file, const char* name_destination_file){
+
+
+
+}
+
+void compress_file(const char* name_source_file, const char* name_destination_file){
+
+
+
+}
+
+void decompress_file(const char* name_source_file, const char* dico, const char* name_destination_file){
+
+
+}
