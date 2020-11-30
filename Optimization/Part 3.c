5564b869fa3896efc0ea50d69a122bfe3719a799
Lindsay Rebeau <lindsay.rebeau@efrei.net>
2020-11-30 01:36:52 +0100

    Add a C source file

---
+++ Optimization/Part 3.c
@@ -0,0 +1,43 @@
+#include "Part 3.h"
+
+void optimization_occurrences(Node* nodes_array[]){
+
+
+
+}
+
+void sort_array_occurrences(){
+
+
+
+}
+
+Node* optimization_create_Huffman_tree (Node** sorted_node_array[], int n){
+
+    return NULL;
+
+}
+
+void organize_nodes_AVL(){
+
+
+
+}
+
+void optimization_compress_file(const char* name_source_file, const char* name_destination_file){
+
+
+
+}
+
+void optimization_decompress_file(const char* name_source_file, const char* dictionary_file, const char* name_destination_file){
+
+
+
+}
+
+void free_dico_tree(Dico_Node* tree){
+
+
+
+}
